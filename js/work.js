dff7b884c5923db694d770a2a34e30d596eca82a
github user name <sensha5172@gmail.com>
2026-01-15 22:32:51 +0900

    aaaa

--- js/work.js
+++ js/work.js
@@ -1,8 +1,12 @@
 // ハンバーガーメニュークリック時にクラス（open）を付け外しする
 const hamburger = document.getElementById("hamburger-btn");
 const sidebar = document.getElementById("side-menu");
+const overlay = document.getElementById("overlay");
+const header = document.getElementById("header");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("open");
   sidebar.classList.toggle("active");
+  overlay.classList.toggle("open");
+  header.classList.toggle("hidden");
 });
